9608fc97f6606b6953b92169c3b86f848b132e80
Brian Leonard <brian@theleoreport.com>
2015-02-04 18:56:52 -0500

    function 6 complete

--- normal.js
+++ normal.js
@@ -76,12 +76,38 @@ function sum(myArray) {
 
 
 function letterIndex(letter) {
-  var n = "abcdefghijklmnopqrstuvwxyz".split("");
-  for (var i = 0; i < 26; i++); {
-    console.log(alphabet);
+  var index;
+  var alphaMap = {
+    "a" : 0,
+    "b" : 1,
+    "c" : 2,
+    "d" : 3,
+    "e" : 4,
+    "f" : 5,
+    "g" : 6,
+    "h" : 7,
+    "i" : 8,
+    "j" : 9,
+    "k" : 10,
+    "l" : 11,
+    "m" : 12,
+    "n" : 13,
+    "o" : 14,
+    "p" : 15,
+    "q" : 16,
+    "r" : 17,
+    "s" : 18,
+    "t" : 19,
+    "u" : 20,
+    "v" : 21,
+    "w" : 22,
+    "x" : 23,
+    "y" : 24,
+    "z" : 25
   }
+  index = alphaMap[letter.toLowerCase()];
+  return index;
 }
-
 
 
 //---------
